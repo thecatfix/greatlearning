500902e90d5aebb772f4f9187f2687aaa7f47893
John Shelburne <john@catfix.biz>
2024-06-27 03:13:37 +0000

    challange for map

---
+++ Code Challanges/map.js
@@ -0,0 +1,11 @@
+var map = new Map
+(
+    [
+        ["first name", "John "],
+        ["last name", "Shelburne"],
+        ["city", "Dallas"],
+        ["mobilenumber",2149070757]
+    ]
+);
+
+console.log(map.keys())
